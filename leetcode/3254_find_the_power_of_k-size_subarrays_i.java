7d59f4c3cc49aeef2f0ec0c930502fd37db1a005
Bao Nguyen <baonlq.97@gmail.com>
2024-11-16 20:16:51 +0700

    add: find the power of k size subarrays i

---
+++ leetcode/3254_find_the_power_of_k-size_subarrays_i.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+class FindThePowerOfKSizeSubarraysI {
+    public static void main(String[] args) {
+        FindThePowerOfKSizeSubarraysI testCase = new FindThePowerOfKSizeSubarraysI();
+        testCase.resultsArray(new int[] { 3,2,3,2,3,2 }, 2);
+    }
+
+    public int[] resultsArray(int[] nums, int k) {
+        var n = nums.length;
+        var resultPair = 0;
+
+        if (n == 1 && k == 1) {
+            return nums;
+        }
+
+        if (n < k) {
+            return new int[] { -1 };
+        }
+
+        for (int i = 0; i <= nums.length - k; i++) {
+            resultPair += 1;
+        }
+
+        
+        var result = new int[resultPair];
+
+        var l = 0;
+        var r = l + k;
+        var pos = 0;
+        var maxVal = 0;
+
+        while (r <= n) {
+            for (int i = l; i < r - 1; i++) {
+                if (nums[i] == nums[i + 1] - 1) {
+                    maxVal = nums[i + 1]; 
+                    continue;
+                }
+                else {
+                    maxVal = -1;
+                    break;
+                }
+            }
+            result[pos] = maxVal;
+            pos++;
+            l++;
+            r++;
+        }
+
+        return result;
+    }
+}
